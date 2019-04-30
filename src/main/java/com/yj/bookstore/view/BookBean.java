c301c15faed9219af4ed9b1c8e978cf8b1476786
starsky1 <763559353@qq.com>
2019-04-30 19:35:23 +0800

    添加编辑、添加、删除功能

--- src/main/java/com/yj/bookstore/view/BookBean.java
+++ src/main/java/com/yj/bookstore/view/BookBean.java
@@ -3,12 +3,17 @@ package com.yj.bookstore.view;
 import com.yj.bookstore.dao.BookDao;
 import com.yj.bookstore.model.domain.Book;
 import lombok.Data;
+import org.primefaces.event.RowEditEvent;
 import org.primefaces.model.LazyDataModel;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * @Author 76355
@@ -21,6 +26,7 @@ import java.io.Serializable;
 public class BookBean implements Serializable{
 
     private Book book;
+    private Book selectedBook;
     @Inject
     private BookDao bookDao;
     @Inject
@@ -28,7 +34,31 @@ public class BookBean implements Serializable{
 
     public BookBean(){
         book=new Book();
-        //lazyModel = new LazyBookDataModel();
+        selectedBook=new Book();
+    }
+
+    public void onRowEdit(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("编辑成功", ((Book) event.getObject()).getBookName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onRowCancel(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("编辑取消", ((Book) event.getObject()).getBookName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onAddNew() {
+        // Add one new car to the table:
+        //bookDao.insertRecord(book);
+        FacesMessage msg = new FacesMessage("图书已添加", book.getBookName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        //清空book的属性值
+        book = new Book();
+    }
+
+    public void onDeleteBook(){
+        FacesMessage msg = new FacesMessage("图书已删除", selectedBook.getBookName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 
 
